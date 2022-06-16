d91ba02fc5296e5593e1aa5b80083d5fb4c65c9d
siegfried bozza <siegfried.bozza@protonmail.com>
2022-06-16 17:15:55 +0200

    added mapbox

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import "../styles/map.css"
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API
+
 
 const Map = () => {
-  return (
-    <div>Map</div>
-  )
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+      map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  return <div ref={mapContainer} className="map-container" ></div>
 }
 
 export default Map
