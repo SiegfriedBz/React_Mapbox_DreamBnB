6694f1f144446b5b1855704d008b25b0e2b434ff
siegfried bozza <siegfried.bozza@protonmail.com>
2022-06-16 23:05:51 +0200

    styled flat card

--- src/components/Flat.js
+++ src/components/Flat.js
@@ -4,17 +4,17 @@ import "../styles/cards.css"
 const Flat = ({flat, selectedFlat}) => {
 
   return (
-    <div className="card card-category ">
-      <img src={flat.imgURL} className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h6 className="card-text">
-        {flat.address}
-        </h6>
-        <p className="card-text">
-        CHF {flat.price}
-        </p>
+    <div className="card card-category">
+      <img src={flat.imgURL} className="card-img-top p-1 rounded-4" alt="..."/>
+      <div className="card-body d-flex flex-column">
+        <div className="d-flex justify-content-center">
+          <div className="fs-6">{flat.address}</div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="fs-6 fw-normal">{flat.price} CHF/day</div>
+        </div>
       </div>
-      <button className='btn btn-primary btn-sm'>Visit</button>
+      <button className='btn btn-primary btn-sm my-1 w-25'>Visit</button>
     </div>
   )
 }
